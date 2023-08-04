98c100fee29b25155d434a90cb67837421e51d65
kishaaggarwal32 <kishaaggarwal195@gmail.com>
2023-08-04 20:40:59 +0530

    added nav and layoutfor home and starred pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Start from './pages/Start';
+import MainLayout from './components/MainLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/starred" element={<Start />}></Route>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/starred" element={<Start />}></Route>
+        </Route>
+
         <Route path="*" element={<div>No Page Found</div>} />
       </Routes>
     </BrowserRouter>
