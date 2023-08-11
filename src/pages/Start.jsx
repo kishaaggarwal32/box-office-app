f27c501f1322692df3879dd519518e57320469a1
kishaaggarwal32 <kishaaggarwal195@gmail.com>
2023-08-11 18:06:34 +0530

    fetched starred shows

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,8 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
 import { useStarredShows } from '../lib/useStarredShows';
+import { getShowsByIds } from '../api/tvmaze';
+import ShowGrid from '../components/shows/ShowGrid';
 
 const Start = () => {
-  const [starredShows] = useStarredShows();
-  return <div>Starred page , starred {starredShows.length} </div>;
+  const [starredShowsIds] = useStarredShows();
+
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      getShowsByIds(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+
+    refetchOnWindowFocus: false,
+  });
+
+  if (starredShows?.length === 0) {
+    return <div>No shows were starred</div>;
+  }
+
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+
+  if (starredShowsError) {
+    return <div>Error Occured:{starredShowsError.message}</div>;
+  }
+  return <div>Shows are loading</div>;
 };
 
 export default Start;
